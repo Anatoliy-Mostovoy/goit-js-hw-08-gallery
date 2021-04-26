6c0205080229a4dce9c948f603fca1785441ca2b
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-04-26 13:23:06 +0300

    Добавил функционал перелистования
    Добавил функционал перелистования картинок в модалке при нажатии навигационных клавишь влево, вправо

--- gallery.js
+++ gallery.js
@@ -5,12 +5,14 @@ const openModal = document.querySelector('.lightbox')
 const pictureModal = document.querySelector('.lightbox__image');
 const closeModal = document.querySelector('.lightbox__button')
 const makeGallery = createGallery(gallerys);
+//todo записываем значение элеиента на котором произошло событие(клик)
+let currentEventLi; 
 
 galleryContainer.insertAdjacentHTML('afterbegin',makeGallery)
 
 galleryContainer.addEventListener('click', onPictureGalleryClick);
 closeModal.addEventListener('click', onCloseModalBtnClick)
-
+//todo создаем раметку
 function createGallery (images){
     return images.map(({preview, original, description})=>{
         return   `
@@ -31,12 +33,11 @@ function onPictureGalleryClick(event){
     if(!event.target.classList.contains('gallery__image')){
         return
     }
+    currentEventLi = event.target.parentNode; //todo обращаеся к родителю елемента на ктором произошло событие - это li
     openModal.classList.add('is-open');
     pictureModal.src = event.target.dataset.source;
     window.addEventListener('keydown', onEscOverlayPres);
     window.addEventListener('keydown', onBtnArrowClick);
-    
-   
 }
 
 function onCloseModalBtnClick(e){
@@ -45,7 +46,6 @@ function onCloseModalBtnClick(e){
     openModal.classList.remove('is-open')
     pictureModal.src = '';
 }
-
 
 const overlay = document.querySelector('.lightbox__overlay')
 overlay.addEventListener('click', onCloseModalBtnClick);
@@ -57,14 +57,27 @@ function onEscOverlayPres(event){
         }
 }
 
+
 function onBtnArrowClick(event){
     if(event.code=== 'ArrowRight'){
-        console.log('Нажал вправо')
+        let pressOnBtnLeft= currentEventLi.nextElementSibling //todo переменная, которая присваивает следующий эоемент(li)
+        //todo проверяем есть ли следующий элемент
+        if(!pressOnBtnLeft){
+        //todo если элеиента нет - подставляем первый
+            pressOnBtnLeft=galleryContainer.firstElementChild
+        }
+        //todo присваиваем текущему значению картинку
+        pictureModal.src = pressOnBtnLeft.lastElementChild.dataset.source
+        currentEventLi = pressOnBtnLeft;
     }
     if(event.code=== 'ArrowLeft'){
-        console.log('Нажал влево')
+        let pressOnBtnRight = currentEventLi.previousElementSibling
+        if(!pressOnBtnRight){
+            pressOnBtnRight=galleryContainer.lastElementChild
+        }
+        pictureModal.src = pressOnBtnRight.lastElementChild.dataset.source
+        currentEventLi = pressOnBtnRight;
     }
     
 }
-// ArrowLeft
-// ArrowRight
+ 
